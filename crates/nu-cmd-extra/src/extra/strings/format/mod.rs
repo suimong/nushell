0a022183bfd93c8f57abec040d5c2b19cd9dac63
132ikl <132@ikl.sh>
2025-03-06 19:06:16 -0500

    Parse actual cell paths in format pattern

--- crates/nu-cmd-extra/src/extra/strings/format/mod.rs
+++ crates/nu-cmd-extra/src/extra/strings/format/mod.rs
@@ -1,7 +1,9 @@
 mod bits;
-mod command;
 mod number;
+mod pattern;
 
-pub(crate) use bits::FormatBits;
-pub(crate) use command::FormatPattern;
-pub(crate) use number::FormatNumber;
+pub(crate) use pattern::FormatPattern;
+// TODO remove `format_bits` visibility after removal of into bits
+pub(crate) use bits::{format_bits, FormatBits};
+// TODO remove `format_number` visibility after removal of into bits
+pub(crate) use number::{format_number, FormatNumber};
