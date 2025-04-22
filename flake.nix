c76f1c14f8be06178b2063604a027758b3b864a0
Ben Yang <ben@ya.ng>
2025-04-22 17:05:04 +0800

    added: essential dev utilities

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A Nix-flake-based Nushell development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+          packages = with pkgs; [ 
+            openssl.dev
+            pkg-config
+          ];
+      };
+    };
+}
